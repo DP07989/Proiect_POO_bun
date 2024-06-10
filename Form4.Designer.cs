de465eb8cdab7a6342e617cd21bf6be3252416d7
Lavric Cosmin-Marian <cosmin.lavric@student.unitbv.ro>
2024-06-10 14:29:43 +0300

    New form

--- Form4.Designer.cs
+++ Form4.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Proiect_POO_bun
 {
-    partial class ModifyUser
+    partial class Form4
     {
         /// <summary>
         /// Required designer variable.
@@ -28,99 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ModifyUser));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form4));
+            this.IDlabel = new System.Windows.Forms.Label();
+            this.IDtextBox = new System.Windows.Forms.TextBox();
             this.ExecuteQueryButton = new System.Windows.Forms.Button();
             this.Titlu = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.PassTextBox = new System.Windows.Forms.TextBox();
             this.NumeTextBox = new System.Windows.Forms.TextBox();
-            this.IDlabel = new System.Windows.Forms.Label();
-            this.IDtextBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // IDlabel
+            // 
+            this.IDlabel.AutoSize = true;
+            this.IDlabel.Font = new System.Drawing.Font("UT Sans Light", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IDlabel.Location = new System.Drawing.Point(82, 118);
+            this.IDlabel.Name = "IDlabel";
+            this.IDlabel.Size = new System.Drawing.Size(20, 20);
+            this.IDlabel.TabIndex = 25;
+            this.IDlabel.Text = "ID";
+            // 
+            // IDtextBox
+            // 
+            this.IDtextBox.Location = new System.Drawing.Point(144, 113);
+            this.IDtextBox.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.IDtextBox.Name = "IDtextBox";
+            this.IDtextBox.Size = new System.Drawing.Size(210, 22);
+            this.IDtextBox.TabIndex = 24;
             // 
             // ExecuteQueryButton
             // 
-            this.ExecuteQueryButton.Location = new System.Drawing.Point(128, 172);
+            this.ExecuteQueryButton.Location = new System.Drawing.Point(194, 236);
             this.ExecuteQueryButton.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
             this.ExecuteQueryButton.Name = "ExecuteQueryButton";
             this.ExecuteQueryButton.Size = new System.Drawing.Size(45, 36);
-            this.ExecuteQueryButton.TabIndex = 15;
+            this.ExecuteQueryButton.TabIndex = 23;
             this.ExecuteQueryButton.Text = "OK";
             this.ExecuteQueryButton.UseVisualStyleBackColor = true;
-            this.ExecuteQueryButton.Click += new System.EventHandler(this.ExecuteQueryButton_Click);
             // 
             // Titlu
             // 
             this.Titlu.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.Titlu.AutoSize = true;
             this.Titlu.Font = new System.Drawing.Font("UT Sans Medium", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Titlu.Location = new System.Drawing.Point(98, 9);
+            this.Titlu.Location = new System.Drawing.Point(128, 68);
             this.Titlu.Name = "Titlu";
-            this.Titlu.Size = new System.Drawing.Size(127, 26);
-            this.Titlu.TabIndex = 14;
-            this.Titlu.Text = "Adauga student";
+            this.Titlu.Size = new System.Drawing.Size(214, 26);
+            this.Titlu.TabIndex = 22;
+            this.Titlu.Text = "Modifica informatii student:";
             this.Titlu.Click += new System.EventHandler(this.Titlu_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("UT Sans Light", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(16, 140);
+            this.label2.Location = new System.Drawing.Point(82, 204);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(44, 20);
-            this.label2.TabIndex = 12;
+            this.label2.TabIndex = 21;
             this.label2.Text = "Parolă";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("UT Sans Light", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(16, 96);
+            this.label1.Location = new System.Drawing.Point(82, 160);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(43, 20);
-            this.label1.TabIndex = 11;
+            this.label1.TabIndex = 20;
             this.label1.Text = "Nume";
             // 
             // PassTextBox
             // 
-            this.PassTextBox.Location = new System.Drawing.Point(78, 135);
+            this.PassTextBox.Location = new System.Drawing.Point(144, 199);
             this.PassTextBox.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
             this.PassTextBox.Name = "PassTextBox";
-            this.PassTextBox.Size = new System.Drawing.Size(210, 27);
-            this.PassTextBox.TabIndex = 9;
+            this.PassTextBox.Size = new System.Drawing.Size(210, 22);
+            this.PassTextBox.TabIndex = 19;
             // 
             // NumeTextBox
             // 
-            this.NumeTextBox.Location = new System.Drawing.Point(78, 91);
+            this.NumeTextBox.Location = new System.Drawing.Point(144, 155);
             this.NumeTextBox.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
             this.NumeTextBox.Name = "NumeTextBox";
-            this.NumeTextBox.Size = new System.Drawing.Size(210, 27);
-            this.NumeTextBox.TabIndex = 8;
+            this.NumeTextBox.Size = new System.Drawing.Size(210, 22);
+            this.NumeTextBox.TabIndex = 18;
             // 
-            // IDlabel
+            // Form4
             // 
-            this.IDlabel.AutoSize = true;
-            this.IDlabel.Font = new System.Drawing.Font("UT Sans Light", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.IDlabel.Location = new System.Drawing.Point(16, 54);
-            this.IDlabel.Name = "IDlabel";
-            this.IDlabel.Size = new System.Drawing.Size(20, 20);
-            this.IDlabel.TabIndex = 17;
-            this.IDlabel.Text = "ID";
-            // 
-            // IDtextBox
-            // 
-            this.IDtextBox.Location = new System.Drawing.Point(78, 49);
-            this.IDtextBox.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
-            this.IDtextBox.Name = "IDtextBox";
-            this.IDtextBox.Size = new System.Drawing.Size(210, 27);
-            this.IDtextBox.TabIndex = 16;
-            // 
-            // ModifyUser
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(298, 271);
+            this.ClientSize = new System.Drawing.Size(426, 364);
             this.Controls.Add(this.IDlabel);
             this.Controls.Add(this.IDtextBox);
             this.Controls.Add(this.ExecuteQueryButton);
@@ -129,14 +128,10 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.PassTextBox);
             this.Controls.Add(this.NumeTextBox);
-            this.Font = new System.Drawing.Font("UT Sans Bold", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.MaximizeBox = false;
-            this.Name = "ModifyUser";
-            this.Text = "Adaugare";
-            this.Load += new System.EventHandler(this.ModifyUser_Load);
+            this.Name = "Form4";
+            this.Text = "Modificare";
+            this.Load += new System.EventHandler(this.Form4_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -144,13 +139,13 @@
 
         #endregion
 
+        private System.Windows.Forms.Label IDlabel;
+        private System.Windows.Forms.TextBox IDtextBox;
         private System.Windows.Forms.Button ExecuteQueryButton;
         private System.Windows.Forms.Label Titlu;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox PassTextBox;
         private System.Windows.Forms.TextBox NumeTextBox;
-        private System.Windows.Forms.Label IDlabel;
-        private System.Windows.Forms.TextBox IDtextBox;
     }
 }
--- Form4.cs
+++ Form4.cs
@@ -1,5 +1,4 @@
-﻿using MySql.Data.MySqlClient;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -11,46 +10,14 @@ using System.Windows.Forms;
 
 namespace Proiect_POO_bun
 {
-    public partial class ModifyUser : Form
+    public partial class Form4 : Form
     {
-        public ModifyUser()
+        public Form4()
         {
             InitializeComponent();
         }
 
-        private void ExecuteQueryButton_Click(object sender, EventArgs e)
-        {
-            if (IDtextBox.Text != "")
-            {
-                MySqlConnection con = new MySqlConnection("server  = localhost; userid = root; password = ; database = poo");
-                con.Open();
-
-                string Query = "UPDATE users SET Name = @user, PassHash = @pass, AL = @AL WHERE ID = @id";
-                MySqlCommand cmd = new MySqlCommand(Query, con);
-                cmd.Parameters.AddWithValue("@id", IDtextBox.Text);
-
-                if (!string.IsNullOrEmpty(NumeTextBox.Text))
-                {
-                    cmd.Parameters.AddWithValue("@user", NumeTextBox.Text);
-                }
-
-                if (!string.IsNullOrEmpty(PassTextBox.Text))
-                {
-                    cmd.Parameters.AddWithValue("@pass", PassTextBox.Text);
-                }
-
-                if (!string.IsNullOrEmpty(ALTextBox.Text))
-                {
-                    cmd.Parameters.AddWithValue("@AL", ALTextBox.Text);
-                }
-
-                cmd.ExecuteNonQuery();
-
-                this.Close();
-            }
-        }
-
-        private void ModifyUser_Load(object sender, EventArgs e)
+        private void Form4_Load(object sender, EventArgs e)
         {
 
         }
